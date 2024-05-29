bc3f66db66c1a4d918b86e1d1af2968fe17cd923
devpablocristo <devpablocristo@gmail.com>
2024-05-29 19:46:43 -0300

    .history removed

--- .history/internal/core/ltp/ltp_20240529145859.go
+++
@@ -1,8 +0,0 @@
-package ltp
-
-type LTP struct {
-	Pair   string  `json:"pair"`
-	Amount float64 `json:"amount"`
-}
-
-type InMemDB map[string]*LTP
--- .history/internal/core/ltp/ltp_20240529155655.go
+++
@@ -1,8 +0,0 @@
-package ltp
-
-type LTP struct {
-	Pair   string      `json:"pair"`
-	Amount interface{} `json:"amount"`
-}
-
-type InMemDB map[string]*LTP
